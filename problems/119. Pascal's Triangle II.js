93dc9f13430cc470538e839ad683237620b6dfc7
chinmaya-kumar-behera <official.chinu2127@gmail.com>
2025-07-14 23:58:58 +0530

    Create 119. Pascal's Triangle II.js

---
+++ problems/119. Pascal's Triangle II.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+
+    const row = new Array(rowIndex + 1).fill(0);
+
+    console.log(row);
+};
+
+const result = getRow(3);
+
+console.log(result);
